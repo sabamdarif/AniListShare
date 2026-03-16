4df0a428408cd128a5131d70658eec00f061bf79
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-03-16 15:15:15 +0530

    test

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+/usr/local/bin/uv venv /vercel/path0/.vercel/python/.venv --python 3.12
+
+/usr/local/bin/uv pip install -r requirements.txt --python /vercel/path0/.vercel/python/.venv/bin/python
+
+source /vercel/path0/.vercel/python/.venv/bin/activate
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
