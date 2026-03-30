0dfc7f9a338a51433f12f80ae75f39e94d2cff44
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-03-30 11:46:34 +0530

    implement mfa and settings page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,10 @@
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
-    path("", include("allauth.urls")),
+    path("settings/", views.account_settings, name="account_settings"),
+    path("delete/", views.account_delete, name="account_delete"),
+    path("", include("allauth.urls"), name="allauth_index"),
+    path("mfa/", include("allauth.mfa.urls"), name="mfa_index"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required
+def account_settings(request):
+    return render(request, "account/settings.html")
+
+@login_required
+def account_delete(request):
+    return render(request, "account/delete.html")
